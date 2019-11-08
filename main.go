de0b489b894a9e5177197c0798e2e528d3b79828
Kim Lewandowski <klewandowski@google.com>
2019-11-08 13:07:33 -0800

    changing txt

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi DevPlats All Hands! Nice gitops, you've requested.")
+		fmt.Fprintf(w, "Hi Michael All Hands! Nice gitops, you've requested.")
 	})
 	fmt.Println("Listening on port 8000")
 	http.ListenAndServe(":8000", nil)
